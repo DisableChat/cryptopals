527038c18b49a4676a37ecd23ee5e8654b57b3b7
Wesley Ryder <wr140815@ohio.edu>
2022-03-14 23:04:59 -0400

    format

--- src/cp.rs
+++ src/cp.rs
@@ -1,25 +1,23 @@
 extern crate rustc_serialize as serialize;
+use core::str;
 use serialize::base64::{self, ToBase64};
 use serialize::hex::{FromHex, ToHex};
-use core::str;
 
 #[allow(dead_code)]
 pub fn hex_to_base64_as_string(hex_input: &str) -> String {
-
-    match hex_input.from_hex(){
+    match hex_input.from_hex() {
         Ok(result) => return result.to_base64(base64::STANDARD),
-        Err(e) => return String::from("Error converting hex to base64 " ) + &e.to_string()
+        Err(e) => return String::from("Error converting hex to base64 ") + &e.to_string(),
     }
 }
 
 // Prob delete this
 #[allow(dead_code)]
 pub fn hex_to_base64(hex_input: &str) -> Vec<u8> {
-
     // Prob need to handle error a better way
-    match hex_input.from_hex(){
+    match hex_input.from_hex() {
         Ok(result) => return result,
-        Err(_) => return Vec::new()
+        Err(_) => return Vec::new(),
     }
 }
 
@@ -44,13 +42,14 @@ pub fn fixed_xor(hex_input_one: &str, hex_input_two: &str) -> String {
 // Also doesn't handle if there is a tie situation
 #[allow(dead_code)]
 pub fn single_byte_xor_cipher(input: &str) -> Result<char, String> {
+    let alphabet: Vec<u8> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .as_bytes()
+        .to_vec();
+    let char_freq: Vec<u8> = "etaoinshrdlu ETAOINSHRDLU".as_bytes().to_vec();
 
-    let alphabet: Vec<u8>    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".as_bytes().to_vec();
-    let char_freq: Vec<u8>   = "etaoinshrdlu ETAOINSHRDLU".as_bytes().to_vec();
-
-    let input                = input.from_hex().expect("from hex error | arg 1 invalid");
-    let mut high_score       = 0;
-    let mut key : char       = ' ';
+    let input = input.from_hex().expect("from hex error | arg 1 invalid");
+    let mut high_score = 0;
+    let mut key: char = ' ';
 
     for letter in alphabet {
         let mut current_score = 0;
@@ -59,7 +58,7 @@ pub fn single_byte_xor_cipher(input: &str) -> Result<char, String> {
         for i in &input {
             let val = i ^ letter;
             if char_freq.contains(&val) {
-               current_score+=1;
+                current_score += 1;
             }
             u8_msg_vec.push(val);
         }
@@ -75,7 +74,6 @@ pub fn single_byte_xor_cipher(input: &str) -> Result<char, String> {
 
 #[allow(dead_code)]
 pub fn orignal_message_as_string(key: &char, message: &str) -> Result<String, String> {
-
     let u8_msg_vec = message.from_hex().expect("from hex error | arg 1 invalid");
     let mut u8_decoded_msg_vec = Vec::new();
     let decoded_message: &str;
@@ -93,33 +91,31 @@ pub fn orignal_message_as_string(key: &char, message: &str) -> Result<String, St
 }
 
 #[allow(dead_code)]
-pub fn single_character_XOR_detect()
-{
+pub fn single_character_xor_detect() {
     //read file and do stuff
 }
 
-
-#[cfg(test)] 
-mod set_one{
+#[cfg(test)]
+mod set_one {
     use super::*;
-#[test]
+    #[test]
     fn test_hex_to_base64_as_string() {
         let input: &str     = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         let expected_result = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
         assert_eq!(hex_to_base64_as_string(input), expected_result);
- 
     }
 
-#[test]
+    #[test]
     fn test_fixed_xor() {
         let arg_one: &str = "1c0111001f010100061a024b53535009181c";
-        let arg_two: &str ="686974207468652062756c6c277320657965";
+        let arg_two: &str = "686974207468652062756c6c277320657965";
         let expected_result = "746865206b696420646f6e277420706c6179";
 
         assert_eq!(fixed_xor(arg_one, arg_two), expected_result);
     }
 
-#[test]
+    #[test]
     fn test_single_byte_xor_cipher() -> Result<(), String> {
         let expected_result = 'X';
         let input: &str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
@@ -128,7 +124,7 @@ mod set_one{
         Ok(())
     }
 
-#[test]
+    #[test]
     fn test_original_message_as_string() -> Result<(), String> {
         let expected_result = "Cooking MC's like a pound of bacon";
         let key = 'X';
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,10 @@ fn main() {
     // Set 1  Challange 3
     let input: &str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
     match cp::single_byte_xor_cipher(input) {
-        Ok(key) => {
-            match cp::orignal_message_as_string(&key, input) {
-                Ok(msg) => println!("Decoded Message: {}", msg),
-                Err(e) => println!("Failed to decode original mssg: {}", e)
-            }
+        Ok(key) => match cp::orignal_message_as_string(&key, input) {
+            Ok(msg) => println!("Decoded Message: {}", msg),
+            Err(e) => println!("Failed to decode original mssg: {}", e),
         },
-        Err(e) => println!("Failed to find key for msg: {}", e)
+        Err(e) => println!("Failed to find key for msg: {}", e),
     };
 }
