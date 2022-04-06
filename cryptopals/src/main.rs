8f932181189e84a548caa13eb2369edbfedf03a2
Wesley Ryder <wr140815@ohio.edu>
2022-04-05 22:52:17 -0400

    wip challenge 6

--- cryptopals/src/main.rs
+++ cryptopals/src/main.rs
@@ -1,16 +1,22 @@
 mod set1;
-#[allow(unused_imports)]
 use set1::cp::*;
+
+use set1::challenge6::*;
+use std::path::Path;
 
 fn main() {
     println!("Cryptopals!");
 
-    /*
     let x = "this is a test".as_bytes();
     let y = "wokka wokka!!!".as_bytes();
     match hamming_distance(x, y) {
-        Ok(res) => print!("result: {:?}", res),
-        Err(e) => print!("Error: {}", e),
+        Ok(res) => print!("result: {:?}\n", res),
+        Err(e) => print!("Error: {}\n", e),
     };
-    */
+    
+    let contents = from_base64_file(Path::new(set1::challenge6::S1C6_FILE)).expect("rip yo contents boi");
+
+    let key_size = find_key_size(contents.as_slice());
+    println!("key_size {}", key_size);
+    // TODO take 4 KEYSIZE blocks instead of 2 and average the distances.
 }
--- cryptopals/src/set1/challenge6.rs
+++ cryptopals/src/set1/challenge6.rs
@@ -1,4 +1,19 @@
 use xor::XOR;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::Read;
+use std::path::Path;
+use std::io::Error;
+extern crate rustc_serialize as serialize;
+extern crate base64;
+use base64::{decode, decode_config_buf};
+#[allow(dead_code)]
+pub static S1C6_FILE: &'static str = "cryptopals/data/set1_challenge6.txt";
+
+
+const MIN_KEYSIZE: usize = 2;
+const MAX_KEYSIZE: usize = 40;
 
 pub fn hamming_distance(x: &[u8], y: &[u8]) -> Result<u32, String> {
     if x.len() != y.len() {
@@ -19,4 +34,42 @@ pub fn differing_bit_count(mut u_8: u8) -> u8 {
         u_8 >>= 1;
     }
     result
+}
+
+pub fn from_base64_file(path: &Path) -> Result<Vec<u8>, Error> {
+    let mut result = Vec::<u8>::new();
+    let file = File::open(&path)?;
+    let buffer = BufReader::new(file);
+    
+    for line in buffer.lines() {
+        decode_config_buf(line.unwrap().trim(), base64::STANDARD, &mut result).unwrap();
+    }
+    // base64::read::DecoderReader - map a read of b64 bytes to decoded bytes, wrap a reader(fil, network sockets, etc)
+    Ok(result)
+}
+
+pub fn find_key_size(input: &[u8]) -> usize {
+    // Let KEYSIZE be the guessed length of the key; try values from 2 to (say) 40.
+    let mut keysize_contenders: Vec<(usize, u32)> = Vec::new();
+    
+    for keysize in  MIN_KEYSIZE..=MAX_KEYSIZE {
+       keysize_contenders.push((keysize, normalized_key_size(input, keysize) as u32));
+    }
+
+    // Er we want to sort by the min hamming_distance in this instance
+    //
+    // TODO: use a fn that is more clear in sorting. Sorting by key and then using the min hamming_distance
+    // seems a bit misleading because the key should be the key size?
+    keysize_contenders.sort_by_key(|k| k.1);
+    println!("Keysize contenders{:?}", keysize_contenders);
+    
+    // Get the first index value's keysize
+    keysize_contenders[0].0
+}
+
+pub fn normalized_key_size(input: &[u8], key_size: usize) -> f32{
+    let chunks: Vec<&[u8]> = input.chunks(key_size).collect();
+    let result = hamming_distance(chunks[0], chunks[1]).expect("Rip the the ham");
+    
+    result as f32 / key_size as f32
 }
