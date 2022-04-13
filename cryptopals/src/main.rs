c324554214d2be68612a1c241aff734b81c8d02e
Wesley Ryder <wr140815@ohio.edu>
2022-04-12 21:13:00 -0400

    need to make single byte xor cipher not use from hex in method rip

--- cryptopals/src/main.rs
+++ cryptopals/src/main.rs
@@ -3,7 +3,7 @@ use set1::cp::*;
 
 use set1::challenge6::*;
 use std::path::Path;
-
+use std::str;
 fn main() {
     println!("Cryptopals!");
 
@@ -20,5 +20,10 @@ fn main() {
     let key_size = find_key_size(contents.as_slice());
     println!("key_size {}", key_size);
 
-    transpose_blocks(contents.as_slice(), key_size);
+    let mut transposed_blocks: Vec<Vec<u8>> = transpose_blocks(contents.as_slice(), key_size);
+    for i in transposed_blocks {
+        let tmp = single_byte_xor_cipher(str::from_utf8(&i).unwrap()).expect("rip");
+        println!("tmp {:?} {:?}", tmp.0, tmp.1);
+    }
+    //transposed_blocks;
 }
